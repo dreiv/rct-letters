9d9175de8f96cc747e632981406969f2926d1497
dreiv <dreivoicu@gmail.com>
2018-08-07 20:41:16 +0300

    fix build

--- src/components/create-comment/CreateComment.js
+++ src/components/create-comment/CreateComment.js
@@ -2,12 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './CreateComment.css'
 
-const propTypes = {}
+const propTypes = {
+	onCreate: PropTypes.func,
+}
 
-const defaultProps = {}
+const defaultProps = {
+	onCreate: () => {},
+}
 
-export default function CreateComment(props) {
-	return <React.Fragment>create comment</React.Fragment>
+export default function CreateComment({ onCreate }) {
+	return (
+		<div>
+			<input type="text" />
+			<button onClick={onCreate}>create</button>
+		</div>
+	)
 }
 
 CreateComment.propTypes = propTypes
